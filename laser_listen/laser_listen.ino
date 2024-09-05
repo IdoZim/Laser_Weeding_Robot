eac0374d5d1765fc4ce302e09165d93f1e8d9c50
IdoZim <idozimry@gmail.com>
2024-09-05 11:55:48 +0300

    #

--- laser_listen/laser_listen.ino
+++ laser_listen/laser_listen.ino
@@ -2,12 +2,14 @@ const int Laser = 8;
 void setup() {
   // put your setup code here, to run once:
   pinMode(Laser, OUTPUT);
+  //digitalWrite(Laser, HIGH);
   Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
   if (Serial.available())
   {
     char command = Serial.read();
